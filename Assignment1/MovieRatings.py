0e6eb321adf0c5940d17457ab776b8c9a5598f6e
Robbert Ruiter <ro-ru@live.nl>
2022-06-30 22:21:16 +0200

    assignment 1 cleanup

--- Assignment1/MovieRatings.py
+++ Assignment1/MovieRatings.py
@@ -9,16 +9,20 @@ class MovieRatings(MRJob):
             MRStep(reducer=self.reducer_sort_by_rating)
         ]
 
+    # For each line of the file, return the movieID
     def mapper_get_movies(self, _, line):
         (userID, movieID, rating, timestamp) = line.split('\t')
         yield movieID, 1
     
+    # Combine the rating and movieID
     def combiner_movies(self, rating, counts):
         yield rating, sum(counts)
 
+    # Reduce the list by counting the ratings per movieID
     def reducer_count_ratings(self, key, values):
         yield None, (sum(values), key)
 
+    # Sort the movieID by rating high->low
     def reducer_sort_by_rating(self, _, rating_counts):
         for count, key in sorted(rating_counts, reverse=True):
             yield (key, int(count))
---
+++ Assignment1/README.md
@@ -0,0 +1,14 @@
+### Student
+Robbert Ruiter
+631188
+
+### Source code
+https://github.com/robbertruiter/PDP/tree/main/Assignment1
+
+### How to run:
+```bash
+$ python MovieRatings.py u.data
+```
+
+### Used file:
+http://witan.nl/hadoop/u.data
